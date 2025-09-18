2a03181405f25c5d9949e5db3c2e954afbbb0e50
brangarten <brandonjgarcia1024@gmail.com>
2025-09-18 18:01:47 -0400

    Hellooo Meowww

---
+++ src/assignment01.java
@@ -0,0 +1,10 @@
+// I'M GOING TO BLOW UP
+// first java thingy bajingy :3
+
+public class assignment01 {
+    public static void main(String[] args) {
+
+        String meow = "Meow, Meowww! :3";
+        System.out.println(meow);
+    }
+}
